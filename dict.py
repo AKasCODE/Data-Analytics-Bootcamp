6b0a812a8455c0abba1d5b0feb70fc1431ae739a
AKasCODE <92628190+AKasCODE@users.noreply.github.com>
2025-07-19 19:21:50 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,24 @@
+                            #Hindi to English dictionary 
+
+# dict={"ek":"one", "do":"two", "teen":"three", "chaar":"four", "panch":"five"}
+# a=input("Ek se lekar panch tak ke beech me ek number likhiye:")
+# b=dict.get(a)
+# print(f"{a} ko English me {b} bolte hai.")
+
+
+                            #Fav lang of 4 friends
+
+# dict={}
+# name=input("Enter your name:")
+# lang=input("Enter your favourate language:")
+# dict.update({name:lang})
+# name=input("Enter your name:")
+# lang=input("Enter your favourate language:")
+# dict.update({name:lang})
+# name=input("Enter your name:")
+# lang=input("Enter your favourate language:")
+# dict.update({name:lang})
+# name=input("Enter your name:")
+# lang=input("Enter your favourate language:")
+# dict.update({name:lang})
+# print(dict)
